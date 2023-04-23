f98b16d8d416e598fd709c5ded602b4a6ab9a071
Ivan Panussis C <ivan.panussis.ing@gmail.com>
2023-04-23 23:57:40 +0100

    Understanding cookies in NextJS

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import { Navbar } from "../ui";
+
+interface Props{
+    children: JSX.Element | JSX.Element[]
+}
+
+export const Layout = ({ children }:Props) => {
+    return(
+        <>
+            <Head>
+
+            </Head>
+            <nav>
+                <Navbar />
+            </nav>
+            <main style={{ padding: '20px 50px'}}>
+                { children }
+            </main>
+        </>
+    );
+}
